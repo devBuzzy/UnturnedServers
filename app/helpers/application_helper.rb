715b43cf92551fccdfee4b295f08724e63faa1c9
Jake <jake0oo0andminecraft@gmail.com>
2014-08-15 09:05:08 -0500

    Finish up graphing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+	def get_time(server, subtract)
+		return server.votes.all.where(created_at: ((Time.now - subtract.day).beginning_of_day..(Time.now - subtract.day).end_of_day)).to_a.size
+	end
 end
