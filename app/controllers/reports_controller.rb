26c45f6f91c447ed8bc2d5d7e45a41aaa4af4983
Jake <jake0oo0andminecraft@gmail.com>
2014-08-15 09:51:01 -0500

    Start on reports

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,38 @@
+class ReportsController < ApplicationController
+	before_filter :authenticate_user!, :except => [:index, :show]
+
+  def new
+  	@server = Server.find(params[:server_id])
+  	@report = @server.reports.new(:user => current_user)
+  end
+
+  def edit
+    @server = Server.find(params[:server_id])
+    @report = @server.reports.find(params[:report_id])
+  end
+
+  def create
+    @server = Server.find(params[:server_id])
+    @report = @server.reports.create(report_params)
+    current_user.reports << @report
+    if @server.save
+      return redirect_to @server, :notice => 'Successfully created report.'
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @server = Server.find(params[:server_id])
+    @reports = server.reports
+  end
+
+  def index
+    @reports = Report.all
+  end
+
+  private
+  def report_params
+    params.require(:report).permit(:reason)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get "reports/new"
+  get "reports/show"
+  get "reports/index"
   root "servers#index"
   resources :servers do
   	resources :comments
+    resources :reports
   	post :cast_vote
   	get :vote
   end
